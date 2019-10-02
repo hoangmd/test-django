af59c0f0bc1056f325d5ae0d34c208a591d36465
Hoang <duchm@fujitsu.com>
2019-10-02 14:22:01 +0700

    1

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from firstapp.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +22,4 @@ urlpatterns = [
 ]
 
 
-hello(repeat=3,repeat_until=None)
+
