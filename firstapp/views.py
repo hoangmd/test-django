0992f5c8577838fcb9605cc97bdc6dbcba83f3a8
Hoang <duchm@fujitsu.com>
2019-10-04 16:01:02 +0700

    2

--- firstapp/views.py
+++ firstapp/views.py
@@ -5,10 +5,13 @@ import requests
 # Create your views here.
 from django.http import HttpResponse
 
+import os
+from django.conf import settings
 
 
 def index(request):
-	with open("a.txt", 'r') as f:
+	file_ = open(os.path.join(settings.BASE_DIR, 'a.txt'))
+	with open(file_, 'r') as f:
 		s = f.readline()
 	return HttpResponse('Hello, World!' + s)
 
