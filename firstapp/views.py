b5665d4e9494d9e0c2d7d4f2b11fd1726b40896f
Hoang <duchm@fujitsu.com>
2019-10-04 15:35:36 +0700

    2

--- firstapp/views.py
+++ firstapp/views.py
@@ -8,15 +8,14 @@ from django.http import HttpResponse
 
 running = False
 a = 0
-def run():
+def start():
 	#r = requests.get(r'https://www.youtube.com/results?search_query=two+steps+from+hell+victory').text
 	while True:
 		a += 1
  
-def index(request):
-	if not running:
-		running = True
-		t = threading.Thread(target=run)
+def index(request, run):
+	if run == 1:
+		t = threading.Thread(target=start)
 		t.start()
 	return HttpResponse('Hello, World!' + str(a))
 
